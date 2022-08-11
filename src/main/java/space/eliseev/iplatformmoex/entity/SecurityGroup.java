686dd6a809c2d241bc71f3721817e847769ede8c
Mark <i.love.rock182@gmail.com>
2022-08-11 23:11:51 +0300

    IM-37 реализован entity SecurityGroup

---
+++ src/main/java/space/eliseev/iplatformmoex/entity/SecurityGroup.java
@@ -0,0 +1,25 @@
+package space.eliseev.iplatformmoex.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+import javax.persistence.*;
+
+@Entity
+@Getter
+@Setter
+public class SecurityGroup {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(length = 93)
+    private String name;
+
+    @Column(length = 756)
+    private String title;
+
+    @Column(name = "is_hidden")
+    private Integer isHidden;
+
+}
