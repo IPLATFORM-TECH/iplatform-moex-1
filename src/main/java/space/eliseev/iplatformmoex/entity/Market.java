1af441a7db3237f355db38d2a30ff4c22a6c5e8b
User <vasa.luk.2001@mail.ru>
2022-08-12 02:47:50 +0300

    IM-32 Реализация entity Market

---
+++ src/main/java/space/eliseev/iplatformmoex/entity/Market.java
@@ -0,0 +1,43 @@
+package space.eliseev.iplatformmoex.entity;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Getter
+@Setter
+@Entity
+@RequiredArgsConstructor
+@Table(name = "market")
+public class Market {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Integer id;
+
+    @Column(name = "trade_engine_id")
+    private Integer tradeEngineId;
+
+    @Column(name = "trade_engine_name")
+    private String tradeEngineName;
+
+    @Column(name = "trade_engine_title")
+    private String tradeEngineTitle;
+
+    @Column(name = "market_name")
+    private String marketName;
+
+    @Column(name = "market_title")
+    private String marketTitle;
+
+    @Column(name = "market_id")
+    private Integer marketId;
+
+    @Column(name = "marketplace")
+    private String marketplace;
+
+    @Column(name = "is_otc")
+    private Integer isOtc;
+}
