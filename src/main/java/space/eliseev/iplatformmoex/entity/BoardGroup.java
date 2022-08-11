063d6a8b9b1974fbba8c69805135e7137cf160e3
romaRacoon <roma1808@inbox.ru>
2022-08-11 20:23:48 +0300

    IM-34 Реализован entity 'BoardGroup'

---
+++ src/main/java/space/eliseev/iplatformmoex/entity/BoardGroup.java
@@ -0,0 +1,51 @@
+package space.eliseev.iplatformmoex.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@Setter
+public class BoardGroup {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(name = "trade_engine_id")
+    private Integer tradeEngineId;
+
+    @Column(name = "trade_engine_name", length = 45)
+    private String tradeEngineName;
+
+    @Column(name = "trade_engine_title", length = 765)
+    private String tradeEngineTitle;
+
+    @Column(name = "market_id")
+    private Integer marketId;
+
+    @Column(name = "market_name", length = 45)
+    private String marketName;
+
+    @Column(name = "name", length = 192)
+    private String name;
+
+    @Column(name = "title", length = 765)
+    private String title;
+
+    @Column(name = "is_default")
+    private Integer isDefault;
+
+    @Column(name = "board_group_id")
+    private Integer boardGroupId;
+
+    @Column(name = "is_traded")
+    private Integer isTraded;
+
+    @Column(name = "is_order_driven")
+    private Integer isOrderDriven;
+
+    @Column(name = "category", length = 45)
+    private String category;
+}
