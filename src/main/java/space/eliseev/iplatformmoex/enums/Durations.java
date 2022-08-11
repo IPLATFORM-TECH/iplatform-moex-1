f400a000b00f5619e130fed0499eebef67d16b5b
User <vasa.luk.2001@mail.ru>
2022-08-12 02:22:05 +0300

    IM-19 Реализация enum Duration

---
+++ src/main/java/space/eliseev/iplatformmoex/enums/Durations.java
@@ -0,0 +1,18 @@
+package space.eliseev.iplatformmoex.enums;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public enum Durations {
+    MINUTE("minute"),
+    TEN_MINUTES("ten_minutes"),
+    HOUR("hour"),
+    DAY("day"),
+    WEEK("week"),
+    MONTH("month"),
+    QUARTAL("quartal");
+
+    private final String name;
+}
