4281d6172bd40886870bcb89a367fc35d4c75262
Mark <i.love.rock182@gmail.com>
2022-08-12 13:47:15 +0300

    IM-21 Реализован enum SecurityGroup

---
+++ src/main/java/space/eliseev/iplatformmoex/model/enumeration/SecurityGroup.java
@@ -0,0 +1,32 @@
+package space.eliseev.iplatformmoex.model.enumeration;
+
+public enum SecurityGroup {
+
+    STOCK_INDEX("stock_index"),
+    STOCK_SHARES("stock_shares"),
+    STOCK_BONDS("stock_bonds"),
+    CURRENCY_SELT("currency_selt"),
+    FUTURES_FORTS("futures_forts"),
+    FUTURES_OPTIONS("futures_options"),
+    STOCK_DR("stock_dr"),
+    STOCK_FOREIGN_SHARES("stock_foreign_shares"),
+    STOCK_EUROBOND("stock_eurobond"),
+    STOCK_PPIF("stock_ppif"),
+    STOCK_ETF("stock_etf"),
+    CURRENCY_METAL("currency_metal"),
+    STOCK_QNV("stock_qnv"),
+    STOCK_GCC("stock_gcc"),
+    STOCK_DEPOSIT("stock_deposit"),
+    CURRENCY_FUTURES("currency_futures"),
+    CURRENCY_INDICES("currency_indices"),
+    STOCK_MORTGAGE("stock_mortgage");
+
+    private final String name;
+    SecurityGroup(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
