b1568f43ef04fc1b514966335983103128630850
Vasiliy Ivanchin <fijirash@yandex.ru>
2022-08-15 17:57:29 +0300

    corretion

--- src/main/java/space/eliseev/iplatformmoex/model/entity/SecurityCollection.java
+++ src/main/java/space/eliseev/iplatformmoex/model/entity/SecurityCollection.java
@@ -25,7 +25,7 @@ public class SecurityCollection {
     private String title;
 
     @ManyToOne
-    @JoinColumn(name = "security_group_id")
+    @JoinColumn(name = "security_group_id", referencedColumnName = "id")
     private SecurityGroup securityGroup;
 
 }
