73004ecd49defe3a18ee921b837fb27462a3ebf2
Vasiliy Ivanchin <fijirash@yandex.ru>
2022-08-15 20:50:58 +0300

    create entity CheckMetadata

---
+++ src/main/java/space/eliseev/iplatformmoex/model/entity/CheckMetadata.java
@@ -0,0 +1,35 @@
+package space.eliseev.iplatformmoex.model.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.Comment;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class CheckMetadata {
+
+    @Id
+    private Long id;
+
+    @Column(name = "timestamp")
+    @Comment("Дата проверки")
+    private LocalDateTime timestamp;
+
+    @Column(name = "index")
+    @Comment("Справочник")
+    private String index;
+
+    @Column(name = "is_valid")
+    @Comment("Результат проверки")
+    private Boolean isValid;
+}
