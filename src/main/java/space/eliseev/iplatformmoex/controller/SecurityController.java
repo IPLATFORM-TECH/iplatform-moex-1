b2bde0e4bbdbb77cdb86bcab3e82932c9221ade9
Dmitry Konovalov <dmitrykonov@mail.ru>
2022-08-27 15:16:13 +0300

    Создал SecurirityClient и  SecurityController

--- src/main/java/space/eliseev/iplatformmoex/controller/SecurityController.java
+++ src/main/java/space/eliseev/iplatformmoex/controller/SecurityController.java
@@ -1,13 +1,30 @@
 package space.eliseev.iplatformmoex.controller;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import space.eliseev.iplatformmoex.service.client.SecurityClient;
+
+import java.util.List;
 
 @RestController
+@RequestMapping("/security")
 public class SecurityController {
 
-    @GetMapping("/security")
-    public Object getSecurity() {
-    return new Object();
+    private SecurityClient securityClient;
+
+    public SecurityController(SecurityClient securityClient) {
+        this.securityClient = securityClient;
+    }
+
+
+    @GetMapping("/getSecurity")
+    public ResponseEntity<List<Object>> getSecurity(@RequestParam("security") String security,
+                                                    @RequestParam(value = "lang", required = false) String lang,
+                                                    @RequestParam(value = "start", required = false) Integer start) {
+
+        return ResponseEntity.ok().body(securityClient.getSecurities(security, lang, start));
     }
 }
---
+++ src/main/java/space/eliseev/iplatformmoex/service/client/SecurityClient.java
@@ -0,0 +1,16 @@
+package space.eliseev.iplatformmoex.service.client;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
+
+@FeignClient(name = "securitiesClient", url = "https://iss.moex.com/iss/securities")
+public interface SecurityClient {
+
+    @GetMapping("/security")
+    List<Object> getSecurities(@RequestParam("security") String security,
+                              @RequestParam(value = "lang", required = false) String lang,
+                              @RequestParam(value = "start", required = false) Integer start);
+}
