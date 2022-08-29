dcf1aeec40a6923c7f2b0b71fc75d47f29754d5b
Vasiliy Ivanchin <fijirash@yandex.ru>
2022-08-29 13:50:28 +0300

    alter mapping EngineComtroller

--- src/main/java/space/eliseev/iplatformmoex/controller/EngineController.java
+++ src/main/java/space/eliseev/iplatformmoex/controller/EngineController.java
@@ -2,6 +2,7 @@ package space.eliseev.iplatformmoex.controller;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import space.eliseev.iplatformmoex.model.entity.Engine;
@@ -10,6 +11,7 @@ import space.eliseev.iplatformmoex.client.EngineClient;
 import java.util.List;
 
 @RestController
+@RequestMapping("/engines")
 public class EngineController {
 
     private final EngineClient engineClient;
@@ -18,7 +20,7 @@ public class EngineController {
         this.engineClient = engineClient;
     }
 
-    @GetMapping("/engines")
+    @GetMapping("/getEngines")
     public ResponseEntity<List<Engine>> getEngines(@RequestParam(defaultValue = "en") String lang) {
         return ResponseEntity.ok().body(engineClient.getEngines(lang));
     }
