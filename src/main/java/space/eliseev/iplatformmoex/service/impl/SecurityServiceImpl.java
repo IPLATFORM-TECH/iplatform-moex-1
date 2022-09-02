fa2f6f556fc5af8c6a8d69e120cc1e1ec0b5321c
MrK103 <i.love.rock182@gmail.com>
2022-09-02 18:35:44 +0300

    IM-69 Изменил алгоритм проверки enum-ов на null

--- src/main/java/space/eliseev/iplatformmoex/service/impl/SecurityServiceImpl.java
+++ src/main/java/space/eliseev/iplatformmoex/service/impl/SecurityServiceImpl.java
@@ -2,29 +2,26 @@ package space.eliseev.iplatformmoex.service.impl;
 
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.stereotype.Service;
 import space.eliseev.iplatformmoex.client.SecurityClient;
 import space.eliseev.iplatformmoex.model.enumeration.Engine;
 import space.eliseev.iplatformmoex.model.enumeration.Market;
 import space.eliseev.iplatformmoex.service.SecurityService;
 
+import java.util.Optional;
+
 @Service
 @RequiredArgsConstructor
 public class SecurityServiceImpl implements SecurityService {
 
     private final SecurityClient securityClient;
-
     @Override
     public Object getSecurities(String q, String lang, Engine engine, Integer isTrading, Market market,
                                 String groupBy, String limit, String groupByFilter, Integer start) {
-        String engineToString;
-        String marketToString;
 
-        if (engine == null) engineToString = "";
-        else engineToString = engine.getName();
-
-        if (market == null) marketToString = "";
-        else marketToString = market.getName();
+        String engineToString = Optional.ofNullable(engine).map(Engine::getName).orElse(StringUtils.EMPTY);
+        String marketToString = Optional.ofNullable(market).map(Market::getName).orElse(StringUtils.EMPTY);
 
         return securityClient.getSecurities(q, lang, engineToString, isTrading,
                 marketToString, groupBy, limit, groupByFilter, start);
