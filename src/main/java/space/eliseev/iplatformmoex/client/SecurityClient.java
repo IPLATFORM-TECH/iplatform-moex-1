cb8bb671a59fbfb81c7dc1ba2685cd4fff62f831
Paul Filippov <filpaul777@gmail.com>
2022-09-06 14:00:31 +0300

    IM-66 добавлен метод getSecurityAggeregates в SecurityController

--- src/main/java/space/eliseev/iplatformmoex/client/SecurityClient.java
+++ src/main/java/space/eliseev/iplatformmoex/client/SecurityClient.java
@@ -7,24 +7,28 @@ import org.springframework.web.bind.annotation.RequestParam;
 import space.eliseev.iplatformmoex.model.enumeration.Engine;
 import space.eliseev.iplatformmoex.model.enumeration.Market;
 
-import java.util.List;
 
-@FeignClient(name="securities", url="${client.post.baseUrl}")
+@FeignClient(name = "securities", url = "${client.post.baseUrl}")
 public interface SecurityClient {
     @GetMapping(".json?q={q}&lang={lang}&engine={engine}&is_trading={is_trading}&market={market}" +
             "&group_by={group_by}&limit={limit}&group_by_filter={group_by_filter}&start={start}")
     Object getSecurities(@PathVariable("q") String q,
-                       @PathVariable("lang") String lang,
-                       @PathVariable("engine") Engine engine,
-                       @PathVariable("is_trading") Integer isTrading,
-                       @PathVariable("market") Market market,
-                       @PathVariable("group_by") String groupBy,
-                       @PathVariable("limit") Integer limit,
-                       @PathVariable("group_by_filter") String groupByFilter,
-                       @PathVariable("start") Integer start);
+                         @PathVariable("lang") String lang,
+                         @PathVariable("engine") Engine engine,
+                         @PathVariable("is_trading") Integer isTrading,
+                         @PathVariable("market") Market market,
+                         @PathVariable("group_by") String groupBy,
+                         @PathVariable("limit") Integer limit,
+                         @PathVariable("group_by_filter") String groupByFilter,
+                         @PathVariable("start") Integer start);
 
-    @GetMapping("/security")
-    List<Object> getSecurity(@RequestParam("security") String security,
+    @GetMapping("/security.json")
+    Object getSecurity(@RequestParam("security") String security,
                              @RequestParam(value = "lang", required = false) String lang,
                              @RequestParam(value = "start", required = false) Integer start);
+
+    @GetMapping(value = "/{security}/aggregates.json?lang={lang}&date={date}")
+    Object getSecurityAggregates(@PathVariable("security") String security,
+                                 @PathVariable("lang") String lang,
+                                 @PathVariable("date") String date);
 }
