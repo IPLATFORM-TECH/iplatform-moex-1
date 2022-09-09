6604a52ecee238bd0ebad632a1263f66726b73fe
Paul Filippov <filpaul777@gmail.com>
2022-09-09 15:23:32 +0300

    IM-60 Добавлена передача аргументов в IndexController.

--- src/main/java/space/eliseev/iplatformmoex/config/UrlConfig.java
+++ src/main/java/space/eliseev/iplatformmoex/config/UrlConfig.java
@@ -1,16 +1,16 @@
-//package space.eliseev.iplatformmoex.config;
-//
-//import org.springframework.beans.factory.annotation.Value;
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.context.annotation.PropertySource;
-//
-//@Configuration
-//@PropertySource("classpath:url-config.yml")
-//public class UrlConfig {
-//
-//    @Value("${feign.engine.url}")
-//    private String engineUrl;
-//
-//    @Value("${feign.securities.url}")
-//    private String securitiesUrl;
-//}
+package space.eliseev.iplatformmoex.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+
+@Configuration
+@PropertySource("classpath:url-config.yml")
+public class UrlConfig {
+
+    @Value("${feign.engine.url}")
+    private String engineUrl;
+
+    @Value("${feign.securities.url}")
+    private String securitiesUrl;
+}
